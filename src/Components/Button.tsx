c7f800150afebcd82d84f2965d87ed68a30183f1
SecludedSoul <pataimasmadthai@gmail.com>
2024-07-31 21:43:31 +0700

    First

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import Button from '@mui/material/Button';
 import SendIcon from '@mui/icons-material/Send';
 import Stack from '@mui/material/Stack';
--- src/Components/UserMenu.tsx
+++ src/Components/UserMenu.tsx
@@ -1,6 +1,4 @@
-import * as React from 'react';
-import Box, { BoxProps } from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Paper from '@mui/material/Paper';
 import UserTable from './Table';
